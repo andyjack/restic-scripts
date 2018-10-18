cc1acab38035c9c94875261402bcdc4a4a7a6e01
Andy Jack <github@veracity.ca>
2018-10-18 18:57:52 -0400

    Keep some recent snapshots too

--- forget.sh
+++ forget.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 SELF_PATH=$( cd -P -- "$( dirname "$0" )" && pwd -P )
-$SELF_PATH/restic.sh forget --keep-daily 7 --keep-weekly 5 --keep-monthly 13 --prune
+$SELF_PATH/restic.sh forget --keep-last 10 --keep-daily 7 --keep-weekly 5 --keep-monthly 13 --prune
